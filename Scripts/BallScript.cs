f4ebed5cb45280691d9415b6c8186d637c3afd5d
Michael Liu <michael.liu95@gmail.com>
2014-09-20 01:32:15 -0400

    ping pong game, bare model

---
+++ Scripts/BallScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    private float grav = 5f;
+
+    private float yMax = -200f;
+
+    private bool collided = false;
+
+    private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        velocity = new Vector3(0f, 100f, 800f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        velocity.y -= grav;
+        if (velocity.y < yMax)
+        {
+            velocity.y = yMax;
+        }
+        this.transform.position += velocity/7000f;
+        Debug.Log(velocity.z);
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (!collided)
+        {
+            if (col.gameObject.name == "Table")
+            {
+                velocity.y *= -1;
+            }
+            if (col.gameObject.name == "NetTable")
+            {
+                velocity.z *= -1;
+            }
+            if (col.gameObject.name == "Player1Swing")
+            {
+                velocity.z *= -1;
+                float temp = col.transform.position.x - this.transform.position.x;
+            }
+        }
+        collided = true;
+    }
+
+    void OnCollisionExit ()
+    {
+        collided = false;
+    }
+}
---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(5, 2);
+
+    public GameObject ballPrefab;
+
+    public bool served = false;
+
+    private GameObject ball;
+
+    private bool swingWait = false;
+
+    private Vector2 movement;
+
+    private Transform paddle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddle = transform.Find("Player1Swing");
+        paddle.collider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("Swing"))
+        {
+            if (!served)
+            {
+                served = true;
+                ball = (GameObject) Instantiate(ballPrefab, new Vector3(paddle.position.x, paddle.position.y, paddle.position.z + .3f), Quaternion.identity);
+            }
+            else
+            {
+                if (!swingWait)
+                {
+                    paddle.collider.enabled = true;
+                    swingWait = true;
+                    Invoke("DisablePaddleCollider", 0.5f);
+                    Invoke("SwingWait", 0.8f);
+                }
+            }
+        }
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+
+    void DisablePaddleCollider()
+    {
+        paddle.collider.enabled = false;
+    }
+
+    void SwingWait()
+    {
+        swingWait = false;
+    }
+}
